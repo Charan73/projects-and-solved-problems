5966d1a80dfa8d2070bb8c38d4eb7c2822c3b849
SRI CHARAN <87242385+Charan73@users.noreply.github.com>
2022-06-28 20:19:08 +0530

    Create delete linked list.cpp

---
+++ solved problems/delete linked list.cpp
@@ -0,0 +1,12 @@
+void deleteNode(LinkedListNode<int> * node) 
+{
+    
+    LinkedListNode<int> * temp = node->next;
+
+  
+    node -> data = temp -> data;
+    node -> next = temp -> next;
+
+ 
+ 
+}
